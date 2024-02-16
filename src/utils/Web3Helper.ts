4e5b466681ec67cf5fb6a55bc0611f48631e3587
Erwan Beauvois <eswak@users.noreply.github.com>
2024-02-16 11:25:09 +0100

    Add FetchAllEvents utils

--- src/utils/Web3Helper.ts
+++ src/utils/Web3Helper.ts
@@ -75,6 +75,102 @@ export async function GetAvgGasPrice(rpcUrl: string) {
   return { r, s, v: hexToNumber(v), deadline: deadline };
 }*/
 
+export async function FetchAllEvents(
+  contract: BaseContract,
+  contractName: string,
+  eventName: string | ContractEventName,
+  startBlock: number,
+  targetBlock: number,
+  blockStepLimit?: number
+): Promise<any[]> {
+  const extractedArray: any[] = new Array();
+  const logPrefix = `fetchAllEvents[${contractName}-${eventName}-all]`;
+
+  const initBlockStep = 100_000;
+  //console.log(`${logPrefix}: will fetch events for ${targetBlock - startBlock + 1} blocks`);
+  let blockStep = blockStepLimit && blockStepLimit < initBlockStep ? blockStepLimit : initBlockStep;
+  let fromBlock = startBlock;
+  let toBlock = 0;
+  let cptError = 0;
+  while (toBlock < targetBlock) {
+    toBlock = fromBlock + blockStep - 1;
+    if (toBlock > targetBlock) {
+      toBlock = targetBlock;
+    }
+
+    let events = undefined;
+    try {
+      events = await contract.queryFilter(eventName, fromBlock, toBlock);
+    } catch (e) {
+      // console.log(`query filter error: ${e.toString()}`);
+      blockStep = Math.round(blockStep / 2);
+      if (blockStep < 1000) {
+        blockStep = 1000;
+      }
+      toBlock = 0;
+      cptError++;
+      if (cptError >= 15) {
+        console.log(`getPastEvents error: ${e}`);
+        throw e;
+      }
+      await sleep(5000);
+      continue;
+    }
+
+    if (events.length != 0) {
+      for (const e of events) {
+        if (e instanceof EventLog) {
+          let obj : any = {
+            transactionHash: e.transactionHash,
+            blockHash: e.blockHash,
+            blockNumber: e.blockNumber,
+            transactionIndex: e.transactionIndex,
+            args: {}
+          };
+          e.fragment.inputs.forEach(function(paramType, i) {
+            obj.args[paramType.name] = e.args[i];
+          });
+          extractedArray.push(obj);
+        } else {
+          throw new Error('Log is not EventLog');
+        }
+      }
+
+      // try to find the blockstep to reach 8000 events per call as the RPC limit is 10 000,
+      // this try to change the blockstep by increasing it when the pool is not very used
+      // or decreasing it when the pool is very used
+      // in any case, should not set the new blockstep to more than 2 times the old one
+      const newBlockStep = Math.min(10_000_000, Math.round((blockStep * 8000) / events.length));
+      if (newBlockStep > blockStep * 2) {
+        blockStep = blockStep * 2;
+      } else {
+        blockStep = newBlockStep;
+      }
+    } else {
+      // if 0 events, multiply blockstep by 2
+      blockStep = blockStep * 2;
+    }
+
+    /*console.log(
+      `${logPrefix}: [${fromBlock} - ${toBlock}] found ${events.length} events after ${cptError} errors (fetched ${
+        toBlock - fromBlock + 1
+      } blocks). Current results: ${extractedArray.length}`
+    );*/
+
+    cptError = 0;
+    fromBlock = toBlock + 1;
+
+    if (blockStepLimit && blockStep > blockStepLimit) {
+      blockStep = blockStepLimit;
+    }
+  }
+
+  /*console.log(
+    `${logPrefix}: found ${extractedArray.length} events in range [${startBlock} ${targetBlock}]`
+  );*/
+  return extractedArray;
+}
+
 export async function FetchAllEventsAndExtractStringArray(
   contract: BaseContract,
   contractName: string,
@@ -88,7 +184,7 @@ export async function FetchAllEventsAndExtractStringArray(
   const logPrefix = `fetchAllEvents[${contractName}-${eventName}-${argNames.join(',')}]`;
 
   const initBlockStep = 100_000;
-  console.log(`${logPrefix}: will fetch events for ${targetBlock - startBlock + 1} blocks`);
+  //console.log(`${logPrefix}: will fetch events for ${targetBlock - startBlock + 1} blocks`);
   let blockStep = blockStepLimit && blockStepLimit < initBlockStep ? blockStepLimit : initBlockStep;
   let fromBlock = startBlock;
   let toBlock = 0;
@@ -146,11 +242,11 @@ export async function FetchAllEventsAndExtractStringArray(
       blockStep = blockStep * 2;
     }
 
-    console.log(
+    /*console.log(
       `${logPrefix}: [${fromBlock} - ${toBlock}] found ${events.length} events after ${cptError} errors (fetched ${
         toBlock - fromBlock + 1
       } blocks). Current results: ${extractedArray.size}`
-    );
+    );*/
 
     cptError = 0;
     fromBlock = toBlock + 1;
@@ -160,8 +256,8 @@ export async function FetchAllEventsAndExtractStringArray(
     }
   }
 
-  console.log(
+  /*console.log(
     `${logPrefix}: found ${extractedArray.size} ${argNames.join(',')} in range [${startBlock} ${targetBlock}]`
-  );
+  );*/
   return Array.from(extractedArray);
 }
