3388882dda49a0ccb12d322e72c0fb30a71016ac
Alfu <Alfulinku@gmail.com>
2024-03-19 17:55:12 +0100

    add notification when timeout on the file mutex

--- src/utils/FileMutex.ts
+++ src/utils/FileMutex.ts
@@ -2,20 +2,30 @@ import fs from 'fs';
 import path from 'path';
 import { DATA_DIR } from './Constants';
 import { sleep } from './Utils';
+import { SendNotifications } from './Notifications';
 
 const lockFile = path.join(DATA_DIR, 'data.lock');
 
 export class FileMutex {
   static async WaitForUnlock() {
     let isLocked = true;
+    const dtStart = Date.now();
     while (isLocked) {
+      const waitDuration = Date.now() - dtStart;
       if (fs.existsSync(lockFile)) {
         const lockedMsg = fs.readFileSync(lockFile, 'utf-8');
         if (lockedMsg.startsWith('LOCKED')) {
+          // do not lock is already locked by same process
+          // this can happen when killing process too harshly
           if (lockedMsg == `LOCKED BY ${process.title}`) {
             isLocked = false;
           } else {
             console.log(`WaitForUnlock: ${lockedMsg}`);
+            if (waitDuration > 15 * 60 * 1000) {
+              // if more than 10 min wait, notify and throw
+              await SendNotifications('FileMutex', 'Timeout', `WaitForUnlock: timeout waiting on ${lockedMsg}`);
+              throw new Error(`WaitForUnlock: timeout waiting on ${lockedMsg}`);
+            }
             await sleep(1000);
           }
         } else {
