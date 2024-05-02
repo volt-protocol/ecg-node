7a00cfea9aad5bfc5f7e7a1b12046c60c484168b
Alfu <Alfulinku@gmail.com>
2024-05-02 11:49:04 +0200

    adds pendle api to get pendle prices
    and get price multi

--- src/model/PendleApi.ts
+++ src/model/PendleApi.ts
@@ -23,3 +23,150 @@ export interface Transaction {
   data: string;
   to: string;
 }
+
+export interface PendleMarketResponse {
+  id: string;
+  chainId: number;
+  address: string;
+  name: string;
+  symbol: string;
+  expiry: Date;
+  pt: Lp;
+  yt: Lp;
+  sy: Lp;
+  lp: Lp;
+  accountingAsset: AccountingAsset;
+  underlyingAsset: AccountingAsset;
+  basePricingAsset: AccountingAsset;
+  rewardTokens: any[];
+  inputTokens: AccountingAsset[];
+  outputTokens: AccountingAsset[];
+  protocol: string;
+  underlyingPool: string;
+  simpleName: string;
+  simpleSymbol: string;
+  simpleIcon: string;
+  proName: string;
+  proSymbol: string;
+  proIcon: string;
+  farmName: string;
+  farmSymbol: string;
+  farmSimpleName: string;
+  farmSimpleSymbol: string;
+  farmSimpleIcon: string;
+  farmProName: string;
+  farmProSymbol: string;
+  farmProIcon: string;
+  assetRepresentation: string;
+  isWhitelistedPro: boolean;
+  isWhitelistedSimple: boolean;
+  votable: boolean;
+  isActive: boolean;
+  isWhitelistedLimitOrder: boolean;
+  accentColor: string;
+  totalPt: number;
+  totalSy: number;
+  totalLp: number;
+  totalActiveSupply: number;
+  liquidity: { [key: string]: number };
+  tradingVolume: TradingVolume;
+  underlyingInterestApy: number;
+  underlyingRewardApy: number;
+  underlyingRewardApyBreakdown: any[];
+  underlyingApy: number;
+  impliedApy: number;
+  ytFloatingApy: number;
+  ptDiscount: number;
+  swapFeeApy: number;
+  pendleApy: number;
+  arbApy: number;
+  aggregatedApy: number;
+  maxBoostedApy: number;
+  lpRewardApy: number;
+  voterApy: number;
+  estimatedDailyPoolRewards: EstimatedDailyPoolReward[];
+  dataUpdatedAt: Date;
+  liquidityChange24h: number;
+  tradingVolumeChange24h: number;
+  underlyingInterestApyChange24h: number;
+  underlyingApyChange24h: number;
+  impliedApyChange24h: number;
+  ytFloatingApyChange24h: number;
+  ptDiscountChange24h: number;
+  swapFeeApyChange24h: number;
+  pendleApyChange24h: number;
+  aggregatedApyChange24h: number;
+  voterApyChange24h: number;
+  categoryIds: string[];
+  timestamp: Date;
+  scalarRoot: number;
+  initialAnchor: number;
+}
+
+export interface AccountingAsset {
+  id: string;
+  chainId: number;
+  address: string;
+  name: string;
+  symbol: string;
+  decimals: number;
+  expiry: null;
+  accentColor: string;
+  price: TradingVolume;
+  priceUpdatedAt: Date;
+  baseType: string;
+  types: string[];
+  protocol: string;
+  simpleName: string;
+  simpleSymbol: string;
+  simpleIcon: string;
+  proName: string;
+  proSymbol: string;
+  proIcon: string;
+  zappable: boolean;
+}
+
+export interface TradingVolume {
+  usd: number;
+}
+
+export interface EstimatedDailyPoolReward {
+  asset: Asset;
+  amount: number;
+}
+
+export interface Asset {
+  id: string;
+  chainId: number;
+  address: string;
+  name: string;
+  symbol: string;
+  decimals: number;
+  accentColor: null;
+  price: TradingVolume;
+  priceUpdatedAt: Date;
+}
+
+export interface Lp {
+  id: string;
+  chainId: number;
+  address: string;
+  name: string;
+  symbol: string;
+  decimals: number;
+  expiry: Date | null;
+  accentColor: string;
+  price: { [key: string]: number };
+  priceUpdatedAt: Date;
+  baseType: string;
+  types: string[];
+  protocol: string;
+  underlyingPool: string;
+  simpleName: string;
+  simpleSymbol: string;
+  simpleIcon: string;
+  proName: string;
+  proSymbol: string;
+  proIcon: string;
+  zappable: boolean;
+}
--- src/utils/Price.ts
+++ src/utils/Price.ts
@@ -1,8 +1,11 @@
+import { getTokenByAddress } from '../config/Config';
 import { DefiLlamaPriceResponse } from '../model/DefiLlama';
+import { PendleMarketResponse } from '../model/PendleApi';
 import SimpleCacheService from './CacheService';
 import { NETWORK } from './Constants';
 import { HttpGet } from './HttpHelper';
 import { Log } from './Logger';
+import { GetWeb3Provider } from './Web3Helper';
 
 export async function GetTokenPriceAtTimestamp(tokenAddress: string, timestamp: number): Promise<number | undefined> {
   // fake prices for sepolia tokens VORIAN and BEEF
@@ -43,14 +46,88 @@ export async function GetTokenPrice(tokenAddress: string): Promise<number | unde
       return 40_000_000_000;
     }
   }
-  const tokenId = NETWORK == 'ARBITRUM' ? `arbitrum:${tokenAddress}` : `ethereum:${tokenAddress}`;
-  const cacheKey = `GetTokenPrice-${tokenId}`;
+
+  let price: number | undefined = undefined;
+  const token = getTokenByAddress(tokenAddress);
+  const cacheKey = `GetTokenPrice-${token.symbol}-${token.address}`;
   const cacheDurationMs = 5 * 60 * 1000; // 5 minute cache duration
+  if (token.pendleConfiguration) {
+    const pendleConfig = token.pendleConfiguration;
+    // fetch price using pendle api
+    price = await SimpleCacheService.GetAndCache(
+      cacheKey,
+      () => GetPendleApiMarketPrice(pendleConfig.market),
+      cacheDurationMs
+    );
 
-  const price = await SimpleCacheService.GetAndCache(cacheKey, () => GetDefiLlamaPrice(tokenId), cacheDurationMs);
+    Log(`GetTokenPrice: price for ${token.symbol} from pendle: ${price}`);
+  } else {
+    const tokenId = NETWORK == 'ARBITRUM' ? `arbitrum:${tokenAddress}` : `ethereum:${tokenAddress}`;
+    const cacheKey = `GetTokenPrice-${tokenId}`;
 
-  Log(`GetTokenPrice: ${tokenId} = $${price}`);
+    price = await SimpleCacheService.GetAndCache(cacheKey, () => GetDefiLlamaPrice(tokenId), cacheDurationMs);
+    Log(`GetTokenPrice: price for ${token.symbol} from llama: ${price}`);
+  }
+
   return price;
+}
+
+export async function GetTokenPriceMulti(tokenAddresses: string[]): Promise<{ [tokenAddress: string]: number }> {
+  const prices: { [tokenAddress: string]: number } = {};
+
+  const defillamaIds: string[] = [];
+  const llamaNetwork = NETWORK == 'ARBITRUM' ? 'arbitrum' : 'ethereum';
+
+  for (const tokenAddress of tokenAddresses) {
+    if (NETWORK == 'SEPOLIA') {
+      if (tokenAddress == '0x50fdf954f95934c7389d304dE2AC961EA14e917E') {
+        // VORIAN token
+        prices[tokenAddress] = 1_000_000_000;
+        continue;
+      }
+      if (tokenAddress == '0x723211B8E1eF2E2CD7319aF4f74E7dC590044733') {
+        // BEEF token
+        prices[tokenAddress] = 40_000_000_000;
+        continue;
+      }
+    }
+
+    const token = getTokenByAddress(tokenAddress);
+
+    if (token.pendleConfiguration) {
+      // fetch price using pendle api
+      prices[tokenAddress] = await GetPendleApiMarketPrice(token.pendleConfiguration.market);
+      Log(`GetTokenPriceMulti: price for ${token.symbol} from pendle: ${prices[tokenAddress]}`);
+      continue;
+    }
+
+    // if here, it means we will fetch price from defillama
+    defillamaIds.push(`${llamaNetwork}:${token.mainnetAddress || token.address}`);
+  }
+
+  if (defillamaIds.length > 0) {
+    const llamaUrl = `https://coins.llama.fi/prices/current/${defillamaIds.join(',')}?searchWidth=4h`;
+    const priceResponse = await HttpGet<DefiLlamaPriceResponse>(llamaUrl);
+
+    for (const tokenAddress of tokenAddresses) {
+      const token = getTokenByAddress(tokenAddress);
+      const llamaId = `${llamaNetwork}:${token.mainnetAddress || token.address}`;
+      const llamaPrice = priceResponse.coins[llamaId] ? priceResponse.coins[llamaId].price : 0;
+
+      prices[tokenAddress] = llamaPrice;
+      Log(`GetTokenPriceMulti: price for ${token.symbol} from llama: ${prices[tokenAddress]}`);
+    }
+  }
+
+  Log(`GetTokenPriceMulti: ends with ${Object.keys(prices).length} prices`);
+  return prices;
+}
+
+async function GetPendleApiMarketPrice(marketAddress: string) {
+  const chainId = (await GetWeb3Provider().getNetwork()).chainId;
+  const pendleApiUrl = `https://api-v2.pendle.finance/core/v1/${chainId}/markets/${marketAddress}`;
+  const response = await HttpGet<PendleMarketResponse>(pendleApiUrl);
+  return response.pt.price.usd;
 }
 
 async function GetDefiLlamaPrice(tokenId: string) {
