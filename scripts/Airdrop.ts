f52d8d2102b5b0b10522725f2b353d5bac1ff6be
Alfu <Alfulinku@gmail.com>
2024-05-14 13:50:48 +0200

    end airdrop data fetch

--- scripts/Airdrop.ts
+++ scripts/Airdrop.ts
@@ -17,6 +17,9 @@ import { GetTokenPriceAtTimestamp } from '../src/utils/Price';
 import { norm } from '../src/utils/TokenUtils';
 import { ProtocolConstants } from '../src/model/ProtocolConstants';
 import { GetGaugeForMarketId } from '../src/utils/ECGHelper';
+import { SurplusGuildMinter__factory } from '../src/contracts/types';
+import { SurplusGuildMinter } from '../src/contracts/types/SurplusGuildMinter';
+import { LoansFileStructure } from '../src/model/Loan';
 
 dotenv.config();
 
@@ -47,16 +50,26 @@ dotenv.config();
 // - borrower airdrop = sum (avg borrow value) / totalBorrowers * 20% * 357143
 // - staker airdrop = sum (avg staked) / totalStakers * 15% * 357143
 
-interface DailyData {
-  marketBalances: MarketBalance[];
-}
-interface MarketBalance {
-  marketId: number;
-  userBalances: UserBalance[];
+const marketSgm: { [marketId: number]: string } = {
+  1: '0xb94aaae7472a694dd959c8497b2f09730391dc52',
+  3: '0x55ab4c8a5f11f8e62d7822d5aed778784df12afd',
+  4: '0x6995aa07b177918423d2127b885b67e7a3cec265'
+};
+
+interface UserDailyData {
+  [userAddress: string]: DailyData;
 }
 
-interface UserBalance {
-  address: string;
+interface DailyData {
+  userAddress: string;
+  dailyBalances: { [dayIso: string]: DayBalance };
+}
+
+interface DayBalance {
+  [marketId: string]: MarketBalance;
+}
+
+interface MarketBalance {
   creditBalanceUsd: number;
   stakedBalanceUsd: number;
   borrowBalanceUsd: number;
@@ -70,7 +83,7 @@ async function computeAirdropData() {
   const endDate = new Date(2024, 4, 17, 12, 0, 0);
   let currentDate = startDate;
   let blockStart = await getBlockAtTimestamp('arbitrum', Math.round(currentDate.getTime() / 1000));
-  const fullData: { [day: string]: DailyData } = {};
+  const fullData: UserDailyData = {};
   while (currentDate < endDate) {
     const stopDate = structuredClone(currentDate);
     stopDate.setDate(stopDate.getDate() + 1);
@@ -90,10 +103,7 @@ async function computeAirdropData() {
       if (config.deployBlock > blockToUse) {
         continue; // ignore market if deployed later
       }
-      const marketBalances: MarketBalance = {
-        marketId: Number(marketId),
-        userBalances: []
-      };
+
       // fetch all addresses who ever held credit
       const creditTransfersFilename = path.join(
         GLOBAL_DATA_DIR,
@@ -102,7 +112,11 @@ async function computeAirdropData() {
         'credit-transfers.json'
       );
 
+      // fetch all loans
+      const loansFilename = path.join(GLOBAL_DATA_DIR, `market_${marketId}`, 'loans.json');
+
       const creditTransferFile: CreditTransferFile = ReadJSON(creditTransfersFilename);
+      const loansFile: LoansFileStructure = ReadJSON(loansFilename);
 
       const uniqueAddresses = Array.from(
         new Set<string>(
@@ -117,6 +131,28 @@ async function computeAirdropData() {
       const guildContract = GuildToken__factory.connect(config.guildTokenAddress, multicallArchivalProvider);
       const liveTerms = await GetGaugeForMarketId(guildContract, Number(marketId), true, blockToUse);
 
+      const surplusGuildMinterContract = SurplusGuildMinter__factory.connect(
+        marketSgm[Number(marketId)],
+        multicallArchivalProvider
+      );
+      const userStakePromises: Promise<SurplusGuildMinter.UserStakeStructOutput>[] = [];
+      for (const userAddress of uniqueAddresses) {
+        for (const termAddress of liveTerms) {
+          userStakePromises.push(
+            surplusGuildMinterContract.getUserStake(userAddress, termAddress, { blockTag: blockToUse })
+          );
+        }
+      }
+
+      const userStakeResults = await Promise.all(userStakePromises);
+
+      const allLoansValue = await Promise.all(
+        loansFile.loans.map((_) => {
+          const lendingTermContract = LendingTerm__factory.connect(_.lendingTermAddress, multicallArchivalProvider);
+          return lendingTermContract.getLoanDebt(_.id, { blockTag: blockToUse });
+        })
+      );
+
       const blockTimestamp = Math.round(stopDate.getTime() / 1000);
       const pegTokenValue = await GetTokenPriceAtTimestamp(config.pegTokenAddress, blockTimestamp);
 
@@ -124,27 +160,49 @@ async function computeAirdropData() {
         throw new Error(`Cannot find peg token price at timestamp ${blockTimestamp}`);
       }
 
-      let cursor = 0;
+      let cursorBalanceOf = 0;
+      let cursorStake = 0;
       for (const address of uniqueAddresses) {
-        const addressBalancePegToken = balanceOfResults[cursor++];
-        if (addressBalancePegToken == 0n) {
-          continue;
+        const addressBalancePegToken = balanceOfResults[cursorBalanceOf++];
+        let userCreditBalance = 0;
+
+        if (addressBalancePegToken != 0n) {
+          userCreditBalance = norm(addressBalancePegToken);
         }
-        const normalizedBalancePegToken = norm(addressBalancePegToken);
-        const balanceInUsd = normalizedBalancePegToken * pegTokenValue;
-        marketBalances.userBalances.push({
-          address,
-          creditBalanceUsd: balanceInUsd,
-          stakedBalanceUsd: 0,
-          borrowBalanceUsd: 0
-        });
+
+        let totalUserStakeCredit = 0;
+        for (const termAddress of liveTerms) {
+          const userStakeForTerm = userStakeResults[cursorStake++];
+          totalUserStakeCredit += norm(userStakeForTerm.credit);
+        }
+
+        let totalUserBorrowValueCredit = 0;
+        for (let cursorLoan = 0; cursorLoan < loansFile.loans.length; cursorLoan++) {
+          const loan = loansFile.loans[cursorLoan];
+          if (loan.borrowerAddress == address) {
+            totalUserBorrowValueCredit += norm(allLoansValue[cursorLoan]);
+          }
+        }
+
+        if (!fullData[address]) {
+          fullData[address] = {
+            userAddress: address,
+            dailyBalances: {}
+          };
+        }
+
+        const dateStr = stopDate.toISOString().split('T')[0];
+        if (!fullData[address].dailyBalances[dateStr]) {
+          fullData[address].dailyBalances[dateStr] = {};
+        }
+
+        fullData[address].dailyBalances[dateStr][`market_${marketId}`] = {
+          creditBalanceUsd: userCreditBalance * pegTokenValue,
+          stakedBalanceUsd: totalUserStakeCredit * pegTokenValue,
+          borrowBalanceUsd: totalUserBorrowValueCredit * pegTokenValue
+        };
       }
 
-      if (!fullData[stopDate.toISOString()]) {
-        fullData[stopDate.toISOString()] = { marketBalances: [] };
-      }
-
-      fullData[stopDate.toISOString()].marketBalances.push(marketBalances);
       WriteJSON('aidrop-data.json', fullData);
     }
 
