52c78fc7ed4488e0bb8cbcc300cf8748ba34258e
Alfu <Alfulinku@gmail.com>
2024-06-27 15:13:36 +0200

    add pendle config in api tokens

--- src/api/model/TokensResponse.ts
+++ src/api/model/TokensResponse.ts
@@ -1,7 +1,10 @@
+import { PendleConfig } from '../../model/Config';
+
 export interface TokensApiInfo {
   address: string;
   symbol: string;
   decimals: number;
   name: string;
   price: number;
+  pendleConfig?: PendleConfig;
 }
